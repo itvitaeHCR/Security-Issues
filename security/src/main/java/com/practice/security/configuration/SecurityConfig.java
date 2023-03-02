d002b0736507e8e427f8deb53c9d184c3717ec80
Hanne Roeland <hanne.roeland@itvitaelearning.nl>
2023-03-02 12:18:53 +0100

    first commit

---
+++ security/src/main/java/com/practice/security/configuration/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.practice.security.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .requestMatchers("/admin")
+                .hasRole("ADMIN")
+
+                .requestMatchers("/user")
+                .hasRole("USER")
+
+                .requestMatchers("/**")
+                .permitAll()
+
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+
+    @Bean
+    // Password encoder
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(
+                User.withUsername("user")
+                        .password(passwordEncoder.encode("wachtwoord"))
+                        .roles("USER")
+                        .build()
+        );
+        manager.createUser(
+                User.withUsername("admin")
+                        .password(passwordEncoder.encode("admin"))
+                        .roles("ADMIN", "USER")
+                        .build()
+        );
+
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
